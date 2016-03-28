9d6aa4aea2aeb92bd47853dcbb2e6ffa5c0b4d85
eyezis <tabhooked@gmail.com>
2016-03-28 20:46:07 +0200

    Initial commit

---
+++ @Resources/Controller.lua
@@ -0,0 +1,78 @@
+function Initialize()
+	inLen = RmGetUInt("BandCount", 25)
+	if inLen <= 0 then inLen = 25 end
+
+	iBarCount = RmGetUInt("BarCount", 200)
+	if iBarCount <= 0 then iBarCount = 200 end
+
+	oMs = {}
+	oMtSource = {}
+	for i=1,inLen do
+		oMs[i] = SKIN:GetMeasure("MsBand" .. i)
+	end
+
+	dataIn = {}
+	dataIn[inLen + 1] = 0
+	dataOut = {}
+	for i=1,iBarCount do
+		dataOut[i] = 0
+	end
+
+	ROI = 12 -- TODO: Make this configurable
+	avgWeight = 1/(ROI*2 + 1)
+end
+
+function Update()
+	for i=1,inLen do
+		dataIn[i] = oMs[i]:GetValue()
+	end
+
+	for i=1,iBarCount do
+		local y = 0
+		local mapped = map(i, 1, iBarCount, 1, inLen)
+		local cur = math.floor(mapped)
+		local weight = mapped - cur
+		
+		dataOut[i] = dataOut[i]*0.2 + interpolate(weight, dataIn[cur], dataIn[cur + 1])*0.8
+	end
+
+	for i=1,iBarCount do
+		local val = 0
+
+		val = val + dataOut[i]
+
+		for j=1,ROI do
+			val = val + dataOut[ clip(i+j, 1, iBarCount) ]
+			val = val + dataOut[ clip(i-j, 1, iBarCount) ]
+		end
+
+		SKIN:Bang("!SetOption", "MsCalc" .. i, "Formula", val * avgWeight)
+	end
+end
+
+function interpolate(weight, v1, v2)
+	return weight * (v2 - v1) + v1
+end
+
+function clip(var, min, max)
+	if var < min then 
+		return min
+	elseif var > max then
+		return max
+	end
+
+	return var
+end
+
+function map(nVar, nMin1, nMax1, nMin2, nMax2)
+	return nMin2 + (nMax2 - nMin2) * ((nVar - nMin1) / (nMax1 - nMin1))
+end
+
+-- Returns a rainmeter variable rounded down to an integer
+function RmGetInt(sVar, iDefault)
+	return math.floor(SKIN:GetVariable(sVar, iDefault))
+end
+-- Returns a rainmeter variable rounded down to an integer, negative integers are converted to positive ones
+function RmGetUInt(sVar, iDefault)
+	return math.abs(RmGetInt(sVar, iDefault))
+end
---
+++ @Resources/Rebuilder.lua
@@ -0,0 +1,179 @@
+function Initialize()
+	-- Class definitions
+	-- iniBuilder: Assists with creating a structured ini
+	iniBuilder = class(function(o)
+		o.tData = {}
+	end)
+	function iniBuilder:NewSection(sSectionName)
+		-- iniSectionBuilder: Sub class of iniBuilder, Assists with creating a structured ini section
+		iniSectionBuilder = class(function(o, sSectionName, oParent)
+			o.oParent = oParent
+			o.tData = {}
+			table.insert(o.tData, ("\[%s\]"):format(sSectionName))
+		end)
+		function iniSectionBuilder:AddKey(sKey, sVal)
+			table.insert(self.tData, ("%s=%s"):format(sKey, sVal))
+		end
+		function iniSectionBuilder:Commit()
+			local iParentSize = #self.oParent.tData
+			for i=1,#self.tData do
+				table.insert(self.oParent.tData, self.tData[i])
+			end
+		end
+
+		return iniSectionBuilder(sSectionName, self)
+	end
+	function iniBuilder:ToString()
+		return table.concat(self.tData, "\n")
+	end
+
+	-- Rebuild the skin if the ini says that we should
+	if RmGetInt("Rebuild", 0) == 1 then
+		Rebuild()
+	end
+end
+
+-- TODO: Figure out if i actually need to leave this here
+function Update()
+end
+
+function Rebuild()
+	-- If rebuild was selected from the context menu then we should refresh the skin (to reload the variables) and then rebuild the meters
+	if RmGetInt("Rebuild", 0) ~= 1 then
+		-- Write a key to the ini so that we know next time that we should call this function to rebuild the meters
+		SKIN:Bang("!WriteKeyValue", "Variables", "Rebuild", "1")
+		SKIN:Bang("!Refresh")
+		return
+	end
+	-- Revert the key to 0 to prevent an infinite loop (which is not as fun as it seems)
+	SKIN:Bang("!WriteKeyValue", "Variables", "Rebuild", "0")
+
+
+	local iBarCount = RmGetUInt("BarCount", 200)
+	if iBarCount <= 0 then iBarCount = 200 end
+
+	local iBandCount = RmGetUInt("BandCount", 40)
+	if iBandCount <= 0 then iBandCount = 40 end
+
+	oMeters = iniBuilder()
+	oMeasures = iniBuilder()
+
+	local o = oMeasures:NewSection("MeasureAudio")
+		o:AddKey("Measure", "Plugin")
+		o:AddKey("Plugin", "AudioLevel")
+		o:AddKey("Port", "Output")
+		o:AddKey("FFTSize", 16384)
+		o:AddKey("FFTOverlap", 10648)
+		o:AddKey("FFTAttack", 64)
+		o:AddKey("FFTDecay", 32)
+		o:AddKey("FreqMin", 30)
+		o:AddKey("FreqMax", 130)
+		o:AddKey("Sensitivity", 20)
+		o:AddKey("Bands", iBandCount + 1)
+	o:Commit()
+
+	for i=1,iBandCount do
+		local o = oMeasures:NewSection("MsBand" .. i)
+			o:AddKey("Measure", "Plugin")
+			o:AddKey("Plugin", "AudioLevel")
+			o:AddKey("Parent", "MeasureAudio")
+			o:AddKey("Type", "Band")
+			o:AddKey("BandIdx", i)
+		o:Commit()
+	end
+
+	for i=1,iBarCount do
+		local o = oMeters:NewSection("MtBar" .. i)
+			o:AddKey("Meter", "Bar")
+			o:AddKey("MeterStyle", "StBar")
+			o:AddKey("MeasureName", "MsCalc" .. i)
+			o:AddKey("X", ("(%s * #BarTotalWidth#)"):format(i - 1))
+		o:Commit()
+
+		local o = oMeasures:NewSection("MsCalc" .. i)
+			o:AddKey("Measure", "Calc")
+			-- TODO:
+			if i <= iBandCount then
+				o:AddKey("Formula", "MsBand" .. i)
+			else
+				o:AddKey("Formula", 0)
+			end
+		o:Commit()
+	end
+
+	local file = io.open(RmGetString("@") .. "Meters.inc", "w+")
+	file:write(oMeters:ToString())
+	file:close()
+
+	local file = io.open(RmGetString("@") .. "Measures.inc", "w+")
+	file:write(oMeasures:ToString())
+	file:close()
+
+	SKIN:Bang("!Refresh")
+end
+
+-- Returns a rainmeter variable rounded down to an integer
+function RmGetInt(sVar, iDefault)
+	return math.floor(SKIN:GetVariable(sVar, iDefault))
+end
+
+-- Returns a rainmeter variable rounded down to an integer, negative integers are converted to positive ones
+function RmGetUInt(sVar, iDefault)
+	return math.abs(RmGetInt(sVar, iDefault))
+end
+
+-- Returns a rainmeter variable represented as a (floating point) number
+function RmGetNumber(sVar, iDefault)
+	return number(SKIN:GetVariable(sVar, iDefault))
+end
+
+-- Returns a rainmeter variable respresented as a string (alias of SKIN:GetVariable)
+function RmGetString(sVar, sDefault)
+	return SKIN:GetVariable(sVar, sDefault)
+end
+
+-- class.lua
+-- Compatible with Lua 5.1 (not 5.0).
+function class(base, init)
+	local c = {}	 -- a new class instance
+	if not init and type(base) == "function" then
+		init = base
+		base = nil
+	elseif type(base) == "table" then
+	 -- our new class is a shallow copy of the base class!
+		for i,v in pairs(base) do
+			c[i] = v
+		end
+		c._base = base
+	end
+	-- the class will be the metatable for all its objects,
+	-- and they will look up their methods in it.
+	c.__index = c
+
+	-- expose a constructor which can be called by <classname>(<args>)
+	local mt = {}
+	mt.__call = function(class_tbl, ...)
+	local obj = {}
+	setmetatable(obj,c)
+	if init then
+		init(obj,...)
+	else 
+		-- make sure that any stuff from the base class is initialized!
+		if base and base.init then
+		base.init(obj, ...)
+		end
+	end
+	return obj
+	end
+	c.init = init
+	c.is_a = function(self, klass)
+		local m = getmetatable(self)
+		while m do 
+			if m == klass then return true end
+			m = m._base
+		end
+		return false
+	end
+	setmetatable(c, mt)
+	return c
+end
