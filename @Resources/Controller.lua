3ae24081e5232a59f6beb1132790ea1c66331b03
EyeZiS <tabhooked@gmail.com>
2016-04-27 22:28:30 +0200

    I really can't remember what i fixed, but it's way better than before

--- @Resources/Controller.lua
+++ @Resources/Controller.lua
@@ -5,27 +5,30 @@ function Initialize()
 	iBarCount = RmGetUInt("BarCount", 200)
 	if iBarCount <= 0 then iBarCount = 200 end
 
+	-- Get all measures and put them in a table for fast access later on
 	oMs = {}
 	for i=1,inLen do
 		oMs[i] = SKIN:GetMeasure("MsBand" .. i)
 	end
 
-	dataIn = {}
-	dataIn[inLen + 1] = 0
-	dataOut = {}
+	dataIn = {} -- Input data
+	dataIn[inLen + 1] = 0 -- Create a zero value out of bounds to prevent errors
+	dataOut = {} -- Output data
 	for i=1,iBarCount do
 		dataOut[i] = 0
 	end
 
-	ROI = 10 -- TODO: Make this configurable
+	ROI = 12 -- Region of influence
 	avgWeight = 1/(ROI*2 + 1)
 end
 
 function Update()
+	-- Get the input data
 	for i=1,inLen do
 		dataIn[i] = oMs[i]:GetValue()
 	end
 
+	-- Interpolate and average the new values
 	for i=1,iBarCount do
 		local y = 0
 		local mapped = map(i, 1, iBarCount, 1, inLen)
@@ -35,6 +38,7 @@ function Update()
 		dataOut[i] = dataOut[i]*0.2 + interpolate(weight, dataIn[cur], dataIn[cur + 1])*0.8
 	end
 
+	-- Average ROI bars forward and backward to create the smooth effect
 	for i=1,iBarCount do
 		local val = 0
 
